ac89ace98f7e5722fd72e43baa5410eedab0103b
koovaaaa <kovas95@gmail.com>
2021-09-17 10:38:21 +0200

    API - Set Swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('Order card online')
+      .setDescription('Web application for online sale of tickets for events in Bosnia and Herzegovina and in the region!')
+      .setVersion('1.0')
+      .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
