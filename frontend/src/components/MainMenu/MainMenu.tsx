8ac59a8d577946f41b36b57200a113e6f6dc4bfc
koovaaaa <kovas95@gmail.com>
2021-09-30 12:37:08 +0200

    Frontend - Add the hash router mechanism

--- frontend/src/components/MainMenu/MainMenu.tsx
+++ frontend/src/components/MainMenu/MainMenu.tsx
@@ -1,16 +1,41 @@
 import React from "react";
 import {Container, Nav} from "react-bootstrap";
+import {HashRouter,Link } from "react-router-dom";
 
-export class MainMenu extends React.Component{
+export class MainMenuItem {
+    text: string = '';
+    link: string = '#';
+
+    constructor(text: string, link: string) {
+        this.text = text;
+        this.link = link;
+    }
+}
+
+interface MainMenuProperties{
+    items: MainMenuItem[];
+}
+
+export class MainMenu extends React.Component <MainMenuProperties>{
+    // constructor(props: MainMenuProperties | Readonly<MainMenuProperties>) {
+    //     super(props);
+    // }
+
     render() {
         return(
             <Container>
                 <Nav variant="tabs">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/">Contact</Nav.Link>
-                    <Nav.Link href="/">Login</Nav.Link>
+                    <HashRouter>
+                        {this.props.items.map(this.makeNavLink)}
+                    </HashRouter>
                 </Nav>
             </Container>
+        );
+    }
+
+    private makeNavLink(item: MainMenuItem){
+        return (
+            <Link to={item.link} className="nav-link"> { item.text }</Link>
         );
     }
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App';
+import HomePage from './components/HomePage/HomePage';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.js';
 import 'popper.js/dist/popper.js'
 import 'bootstrap/dist/js/bootstrap.min.js';
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
-import {MainMenu} from "./components/MainMenu/MainMenu";
+import {MainMenu, MainMenuItem} from "./components/MainMenu/MainMenu";
+import {HashRouter, Route, Switch} from "react-router-dom";
+import ContactPage from "./components/ContactPage/ContactPage";
+import UserLoginPage from "./components/UserLoginPage/UserLoginPage";
 
-
+const menuItems = [
+    new MainMenuItem("Home", "/"),
+    new MainMenuItem("Choose category", "/category/"),
+    new MainMenuItem("Choose country", "/country/"),
+    new MainMenuItem("Choose city", "/city"),
+    new MainMenuItem("About us", "/page/about-us/"),
+    new MainMenuItem("Contact", "/contact/"),
+    new MainMenuItem("Login", "/login/")
+]
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainMenu />
-    <App />
+      <MainMenu items={ menuItems }></MainMenu>
+      <HashRouter>
+          <Switch>
+              <Route exact path="/" component={ HomePage } />
+              <Route path="/contact" component={ ContactPage } />
+              <Route path="/login" component={ UserLoginPage } />
+          </Switch>
+      </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
