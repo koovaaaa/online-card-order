4eb87aa67d1af4d59087fe5c44cdf0edf48042d1
koovaaaa <kovas95@gmail.com>
2021-10-15 01:10:27 +0200

    Frontend - Implement page for Event Tickets

--- frontend/src/components/user/ticketsPage.jsx
+++ frontend/src/components/user/ticketsPage.jsx
@@ -1,11 +1,50 @@
 import {Component} from "react";
+import api from "../../api/api";
+import {Button, Container, Table} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faShoppingCart} from "@fortawesome/free-solid-svg-icons";
 
 export class TicketsPage extends Component {
     state = {
-        eventId: this.props.match.params.id
+        eventId: this.props.match.params.id,
+        tickets: [],
+        eventName: '',
+        city: '',
     }
 
+    async componentDidMount() {
+        const tickets = await api(`user-ticket/get-event-tickets/${this.state.eventId}`, 'get', '');
+        const event = await api(`user-event/get-event/${this.state.eventId}`, 'get', '');
+        await this.setState({tickets, eventName: event.eventName, city: event.city.cityName});
+    }
+
+
     render() {
-        return 'Ulaznice..' + this.state.eventId;
+        return (
+            <Container>
+                <h1 className={"fw-bold"}>{this.state.eventName} @ {this.state.city}</h1>
+                <Table hover className={"table table-borderless"}>
+                    <thead className={"border-bottom"}>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th className={"text-md-end"}>Broj ulaznica na stanju</th>
+                        <th className={"text-md-end"}>Cijena</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.tickets.map(ticket => <tr key={ticket.ticket.ticketId}>
+                        <td><b>{ticket.ticket.ticketName.toUpperCase()}</b></td>
+                        <td className={"text-md-start"}>{ticket.ticket.description}</td>
+                        <td className={"text-md-end"}>{ticket.ticket.ticketCount}</td>
+                        <td className={"text-md-end"}>{ticket.price}</td>
+                        <td className={"text-md-center justify-content-around"}><Button
+                            variant={"outline-primary"}><FontAwesomeIcon icon={faShoppingCart}/> Naruči kartu</Button>
+                        </td>
+                    </tr>)}
+                    </tbody>
+                </Table>
+            </Container>
+        );
     }
 }
