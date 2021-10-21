eabc372a47101f8dc4e7026f4136f4ba03fe7f91
koovaaaa <kovas95@gmail.com>
2021-10-21 22:15:45 +0200

    Frontend - Update homepage

--- frontend/src/components/guest/homePage.jsx
+++ frontend/src/components/guest/homePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import api from "../../api/api";
-import {Carousel} from "react-bootstrap";
+import {Carousel, Col, Container, FormText, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 export class HomePage extends React.Component {
@@ -19,29 +19,62 @@ export class HomePage extends React.Component {
         this.setState({index: selectedIndex});
     };
 
+    imageClicked(event) {
+        this.handleSelect(+event.target.id);
+    }
+
+    checkIndex(index) {
+        return index === this.state.index;
+    }
+
     render() {
         const {events, index} = this.state;
         return (
+            <>
+                <Row>
+                    <Col xs={10}>
+                        <Carousel fade activeIndex={index} onSelect={this.handleSelect}>
+                            {events.map(event =>
+                                <Carousel.Item key={event.eventId}>
+                                    <Link to={`event/${event.eventId}`}>
+                                        <img
+                                            className="w-100 rounded"
+                                            height={'615px'}
+                                            src={process.env.REACT_APP_API_URL + event.eventPhoto}
+                                            alt="First slide"
+                                        />
+                                        <Carousel.Caption
+                                            className={' border-3 rounded border-radius-2  carousel-caption '}>
+                                            <h2>{event.eventName}</h2>
+                                            <h4 className={'fw-bold'}>{event.eventDate}</h4>
+                                        </Carousel.Caption>
+                                    </Link>
+                                </Carousel.Item>
+                            )}
+                        </Carousel>
+                        <FormText className={'small fw-bold'}>Napomena: Na Početnoj stranici je
+                            prikazano
+                            pet narednih događaja! Sve događaje možete pogledati
+                            <Link to={'events'} className={'text-decoration-none'}> ovdje!</Link></FormText>
+                    </Col>
+                    <Col>
+                        {events.map((event, index) =>
+                            <Container key={event.eventId}>
+                                <img
+                                    src={process.env.REACT_APP_API_URL + event.eventPhoto}
+                                    className={'w-100 rounded'} height={'105px'}
+                                    id={index}
+                                    border={this.checkIndex(index) ? '4px' : ''}
+                                    onClick={event => this.imageClicked(event)}
+                                    alt={'eventPhoto'}
+                                />
+                                <br/><br/>
+                            </Container>
+                        )}
 
-            <Carousel fade activeIndex={index} onSelect={this.handleSelect}>
-                {events.map(event =>
-                    <Carousel.Item key={event.eventId}>
-                        <Link to={`event/${event.eventId}`}>
-                            <img
-                                className="d-block w-100"
-                                src={process.env.REACT_APP_API_URL + event.eventPhoto}
-                                alt="First slide"
-                            />
-                            <Carousel.Caption
-                                className={' border-3 rounded border-radius-2  carousel-caption '}>
-                                <h2>{event.eventName}</h2>
-                                <h5 className={'fw-bold'}>{event.description}</h5>
-                                <h4 className={'fw-bold'}>{event.eventDate}</h4>
-                            </Carousel.Caption>
-                        </Link>
-                    </Carousel.Item>
-                )}
-            </Carousel>
+                    </Col>
+                </Row>
+            </>
 
 
         );
