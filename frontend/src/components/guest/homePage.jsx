11c60885d64f25c4fe05f81ceb4da17cb81f60b6
koovaaaa <kovas95@gmail.com>
2021-10-21 14:05:28 +0200

    Update homePage.jsx

--- frontend/src/components/guest/homePage.jsx
+++ frontend/src/components/guest/homePage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import api from "../../api/api";
-import {CardGroup, Col, Row} from "react-bootstrap";
-import EventCard from "./eventCard";
+import {Carousel} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 export class HomePage extends React.Component {
     state = {
-        events: []
+        events: [],
+        index: 1
     }
 
     async componentDidMount() {
@@ -14,20 +15,35 @@ export class HomePage extends React.Component {
         await this.setState({events: events.events})
     }
 
+    handleSelect = (selectedIndex) => {
+        this.setState({index: selectedIndex});
+    };
+
     render() {
+        const {events, index} = this.state;
         return (
-            <Row>
-                {this.state.events.map(event =>
-                    <Col key={event.eventId}>
-                        <CardGroup>
-                            <EventCard eventId={event.eventId}
-                                       eventName={event.eventName}
-                                       eventPhoto={event.eventPhoto}
-                                       country={event.country.countryName} city={event.city.cityName}
-                                       eventDate={event.eventDate}/>
-                        </CardGroup>
-                    </Col>)}
-            </Row>
+
+            <Carousel fade activeIndex={index} onSelect={this.handleSelect}>
+                {events.map(event =>
+                    <Carousel.Item key={event.eventId}>
+                        <Link to={`event/${event.eventId}`}>
+                            <img
+                                className="d-block w-100"
+                                src={process.env.REACT_APP_API_URL + event.eventPhoto}
+                                alt="First slide"
+                            />
+                            <Carousel.Caption
+                                className={' border-3 rounded border-radius-2  carousel-caption '}>
+                                <h2>{event.eventName}</h2>
+                                <h5 className={'fw-bold'}>{event.description}</h5>
+                                <h4 className={'fw-bold'}>{event.eventDate}</h4>
+                            </Carousel.Caption>
+                        </Link>
+                    </Carousel.Item>
+                )}
+            </Carousel>
+
+
         );
     }
 }
